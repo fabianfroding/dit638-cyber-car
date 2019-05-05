11e8387c8dd991680c36ae0b0c09aa9078094c5d
olivermanzi <olivermanzi.personal@gmail.com>
2019-05-05 21:52:53 +0200

    [wip] updated messages (del and acc) and fixed up delegator micro service - added prints that display all messages recived and sent to the delegator - updated messages o have a 'front_sensor_is_safe' boolean - none yet

--- microservices/delegator/src/delegator.cpp
+++ microservices/delegator/src/delegator.cpp
@@ -23,18 +23,16 @@ int32_t main(int32_t argc, char **argv)
         std::cerr << argv[0] << "[--carlos=<ID of carlos microservices>]" << std::endl;
         std::cerr << argv[0] << "[--freq=<frequency>]" << std::endl;
         std::cerr << argv[0] << "[--verbose" << std::endl;
-        std::cerr << argv[0] << "[--extra_v" << std::endl;
         std::cerr << argv[0] << "[--help]" << std::endl;
-        std::cerr << "example:  " << argv[0] << " --cid=112 --carlos=646" << std::endl;
+        std::cerr << "example:  " << argv[0] << " --cid=112 --carlos=113 --freq=30" << std::endl;
         return -1;
     }
     const uint16_t CARLOS_SESSION{(commandlineArguments.count("carlos") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["carlos"])) : static_cast<uint16_t>(113)};
     const uint16_t CID_SESSION{(commandlineArguments.count("cid") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["cid"])) : static_cast<uint16_t>(112)};
     const float FREQ{(commandlineArguments.count("freq") != 0) ? static_cast<float>(std::stof(commandlineArguments["freq"])) : static_cast<float>(30)};
     const bool VERBOSE{commandlineArguments.count("verbose") != 0};
-    const bool EXTRA_VERBOSE{commandlineArguments.count("extra_v") != 0};
 
-    if (VERBOSE || EXTRA_VERBOSE)
+    if (VERBOSE)
     {
         std::cout << "starting up " << argv[0] << "..." << std::endl;
     }
@@ -49,31 +47,35 @@ int32_t main(int32_t argc, char **argv)
 
     if (car_session.isRunning())
     {
-        if (VERBOSE || EXTRA_VERBOSE)
+        if (VERBOSE)
         {
             std::cout << "[delegator] micro-service started..." << std::endl;
         }
 
         /*global variables*/
-        float speed = NULL, turn = NULL;
-        float center_of_car = NULL, area_of_car = nullptr;
-        float sign_type = NULL, center_of_sign = NULL, area_of_sign = NULL;
+        float unassigned = -1.0;
+        bool front_sensor = false;                                    //acc
+        float speed = unassigned, turn = unassigned;                  //acc
+        float center_of_car = unassigned, area_of_car = unassigned;   //car vision
+        float center_of_sign = unassigned, area_of_sign = unassigned; //sign vision
+        uint16_t sign_type = -1;                                      //sign vision
 
         opendlv::proxy::PedalPositionRequest pedal;
         opendlv::proxy::GroundSteeringReading wheel;
 
         /* prepare messages to recieve from carlos session */
-        auto adaptive_cruise_control = [VERBOSE, &pedal, &speed](cluon::data::Envelope &&envelope) {
+        auto adaptive_cruise_control = [VERBOSE, &pedal, &speed, &front_sensor](cluon::data::Envelope &&envelope) {
             /** unpack message recieved*/
             auto msg = cluon::extractMessage<carlos::acc>(std::move(envelope));
-            /*store speed value from acc microservice*/
+            /*store speed and front_sensor value from acc microservice*/
             speed = msg.speed();
+            front_sensor = msg.front_sensor_is_safe();
             /*store speed in car object*/
             pedal.position(speed);
 
             if (VERBOSE)
             {
-                std::cout << "set speed to [" << speed << "]" << std::endl;
+                std::cout << "RECIVED -> front sensor distance: [" << front_sensor << "], speed set to [" << speed << "]" << std::endl;
             }
         };
 
@@ -87,26 +89,35 @@ int32_t main(int32_t argc, char **argv)
 
             if (VERBOSE)
             {
-                std::cout << "set wheel to [" << turn << "]" << std::endl;
+                std::cout << "RECIVED -> wheel set to [" << turn << "]" << std::endl;
             }
         };
 
-        auto car_detection_color = [VERBOSE, &wheel, &turn, &pedal, &speed, &center_of_car, &area_of_car](cluon::data::Envelope &&envelope) {
+        auto car_detection_color = [VERBOSE, /* &wheel, &turn, &pedal, &speed, */ &center_of_car, &area_of_car](cluon::data::Envelope &&envelope) {
             /** unpack message recieved*/
             auto msg = cluon::extractMessage<carlos::vision::car>(std::move(envelope));
             /*store data*/
             center_of_car = msg.coc();
             area_of_car = msg.area();
             /*do stuff with data*/
+            if (VERBOSE)
+            {
+                std::cout << "RECIVED -> coc (wheel): [" << center_of_car << "], area of car: [" << area_of_car << "]" << std::endl;
+            }
         };
 
-        auto sign_detection_color = [VERBOSE, &pedal, &speed, &center_of_sign, &area_of_sign](cluon::data::Envelope &&envelope) {
+        auto sign_detection_color = [VERBOSE, /* &pedal, &speed, */ &center_of_sign, &area_of_sign, &sign_type](cluon::data::Envelope &&envelope) {
             /** unpack message recieved*/
             auto msg = cluon::extractMessage<carlos::vision::sign>(std::move(envelope));
             /*store data*/
-            center_of_sign = msg.coc();
+            center_of_sign = msg.cos();
             area_of_sign = msg.area();
+            sign_type = msg.type();
             /*do stuff with data*/
+            if (VERBOSE)
+            {
+                std::cout << "RECIVED -> cos: [" << center_of_sign << "], area of sign: [" << area_of_sign << "]" << std::endl;
+            }
         };
 
         /*registers callbacks*/
@@ -122,17 +133,31 @@ int32_t main(int32_t argc, char **argv)
 
             if (VERBOSE)
             {
-                std::cout << "set speed to [" << pedal.position() << "]" << std::endl;
+                std::cout << "SENT -> set speed to [" << pedal.position() << "]" << std::endl;
+            }
+            return true;
+        }};
+
+        auto sendWheelRequest{[VERBOSE, &car_session, &wheel]() -> bool {
+            /*send pedal req object to car*/
+            car_session.send(wheel);
+
+            if (VERBOSE)
+            {
+                std::cout << "SENT -> set wheel to [" << wheel.groundSteering() << "]" << std::endl;
             }
             return true;
         }};
 
         /*register time triggers*/
         car_session.timeTrigger(FREQ, sendPedalRequest);
+        car_session.timeTrigger(FREQ, sendWheelRequest);
 
         while (car_session.isRunning())
         {
             /* just run this microservice until ist crashes */
+            // car_session.send(pedal);
+            // car_session.send(wheel);
         }
     }
     else

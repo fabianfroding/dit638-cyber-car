bc860a904db3703a5e4228d0b63ffa37edc3c0da
olivermanzi <olivermanzi.personal@gmail.com>
2019-05-06 21:48:46 +0200

    [wip] integrated semaphore into ACC - wrapped the code that is sent to the car in a semaphore - hooked the semaphore to the delegator class - updated the messages

--- microservices/control/acc/src/acc.cpp
+++ microservices/control/acc/src/acc.cpp
@@ -13,6 +13,120 @@
 
 float autoPedal(float front_sensor, float SAFE_DISTANCE, float MAX_SPEED, bool VERBOSE);
 
+/*main*/
+int32_t main(int32_t argc, char **argv)
+{
+    /**Parse the arguments from the command line*/
+    auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+
+    if (0 != commandlineArguments.count("help"))
+    {
+        std::cerr << argv[0] << " is an example application for miniature vehicles (Kiwis) of DIT638 course." << std::endl;
+        std::cerr << "Usage:" << argv[0] << " --cid=<CID of your OD4Session>" << std::endl;
+        std::cerr << argv[0] << "[--carlos=<ID of carlos microservices>]" << std::endl;
+        std::cerr << argv[0] << "[--sd=<front/back safety space>]" << std::endl;
+        std::cerr << argv[0] << "[--sp=<speed, min is 0.13 and max is 0.8>]" << std::endl;
+        std::cerr << argv[0] << "[--help]" << std::endl;
+        std::cerr << "example:  " << argv[0] << " --cid=112 --sd=0.2 --sp=013" << std::endl;
+        return -1;
+    }
+    const uint16_t CARLOS_SESSION{(commandlineArguments.count("carlos") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["carlos"])) : static_cast<uint16_t>(113)};
+    const uint16_t CID{(commandlineArguments.count("cid") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["cid"])) : static_cast<uint16_t>(112)};
+    const bool VERBOSE{commandlineArguments.count("verbose") != 0};
+    const float SAFE_DISTANCE{(commandlineArguments.count("sd") != 0) ? static_cast<float>(std::stof(commandlineArguments["sd"])) : static_cast<float>(0.30)};
+    const float MAX_SPEED{(commandlineArguments.count("sp") != 0) ? static_cast<float>(std::stof(commandlineArguments["sp"])) : static_cast<float>(0.15)};
+
+    if (VERBOSE)
+    {
+        std::cout << "starting up " << argv[0] << "..." << std::endl;
+        std::cout << "speed: [" << MAX_SPEED << "], front saftey distance: [" << SAFE_DISTANCE << " meters]" << std::endl;
+    }
+
+    /**
+     * create a od4session object that will allow all microservices
+     * with the same secret number to send and recieve messages from
+     * one another
+    */
+
+    cluon::OD4Session carlos_session{CARLOS_SESSION}; //needed to send messages to carlos session
+    cluon::OD4Session car_session{CID};               //needed to recieve data from sensors
+
+    if (car_session.isRunning())
+    {
+        if (VERBOSE)
+        {
+            std::cout << "[acc] micro-service started..." << std::endl;
+        }
+
+        carlos::acc acc_status;                     //danger (if distance is below saftey distance)
+        opendlv::proxy::PedalPositionRequest pedal; //position (speed of vehicle)
+
+        bool SEMAPHORE_KEY = true;
+
+        /*sends front sensor messages to carlos delegator*/
+        auto sensors = [VERBOSE, SAFE_DISTANCE, MAX_SPEED, &car_session, &carlos_session, &pedal](cluon::data::Envelope &&envelope) {
+            /** unpack message recieved*/
+            auto msg = cluon::extractMessage<opendlv::proxy::DistanceReading>(std::move(envelope));
+            /*store sender id*/
+            const uint16_t senderStamp = envelope.senderStamp();
+            /*store sender data*/
+            float distance = msg.distance();
+
+            if (senderStamp == 0)
+            {
+                /**
+                 * only get data fron sender 0 (front sensor)
+                 * */
+
+                /*get front sensor value*/
+                float frontSensor = distance;
+                /*do stuff with data*/
+
+                /*pedal*/
+                float speed = autoPedal(frontSensor, SAFE_DISTANCE, MAX_SPEED, VERBOSE);
+                pedal.position(speed);
+                if (SEMAPHORE_KEY)
+                {
+                    car_session.send(pedal);
+                }
+
+                /*acc status*/
+                acc_status.safe_to_drive((frontSensor > SAFE_DISTANCE) ? true : false); //if front sensor is safe, set to true, else set to false
+                /*send object to carlos delegator*/
+                carlos_session.send(acc_status);
+            }
+        };
+
+        auto semaphore = [VERBOSE, &carlos_session, &SEMAPHORE_KEY](cluon::data::Envelope &&envelope) {
+            /** unpack message recieved*/
+            auto msg = cluon::extractMessage<carlos::semaphore::acc>(std::move(envelope));
+            /*store data*/
+            SEMAPHORE_KEY = msg.semaphore();
+
+            if (VERBOSE)
+            {
+                std::cout << "RECIEVED -> SEMAPHORE_KEY [" << SEMAPHORE_KEY << "]" << std::endl;
+            }
+        };
+
+        /*registers callbacks*/
+        car_session.dataTrigger(opendlv::proxy::DistanceReading::ID(), sensors);
+        car_session.dataTrigger(carlos::semaphore::acc::ID(), semaphore);
+
+        while (car_session.isRunning())
+        {
+            /* just run this microservice until ist crashes */
+        }
+    }
+    else
+    {
+        std::cout << "Carlos Out. (OD4Session timed out.)" << std::endl;
+    }
+    return 0;
+}
+
+/*functions*/
+
 float autoPedal(float front_sensor, float SAFE_DISTANCE, float MAX_SPEED, bool VERBOSE)
 {
     const float neutral = 0.0;
@@ -46,100 +160,4 @@ float autoPedal(float front_sensor, float SAFE_DISTANCE, float MAX_SPEED, bool V
         }
     }
     return result;
-}
-
-int32_t main(int32_t argc, char **argv)
-{
-    /**Parse the arguments from the command line*/
-    auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
-
-    if (0 != commandlineArguments.count("help"))
-    {
-        std::cerr << argv[0] << " is an example application for miniature vehicles (Kiwis) of DIT638 course." << std::endl;
-        std::cerr << "Usage:" << argv[0] << " --cid=<CID of your OD4Session>" << std::endl;
-        std::cerr << argv[0] << "[--carlos=<ID of carlos microservices>]" << std::endl;
-        std::cerr << argv[0] << "[--sd=<front/back safety space>]" << std::endl;
-        std::cerr << argv[0] << "[--sp=<speed, min is 0.13 and max is 0.8>]" << std::endl;
-        std::cerr << argv[0] << "[--help]" << std::endl;
-        std::cerr << "example:  " << argv[0] << " --cid=112 --sd=0.2 --sp=013" << std::endl;
-        return -1;
-    }
-    const uint16_t CARLOS_SESSION{(commandlineArguments.count("carlos") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["carlos"])) : static_cast<uint16_t>(113)};
-    const uint16_t CID{(commandlineArguments.count("cid") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["cid"])) : static_cast<uint16_t>(112)};
-    const bool VERBOSE{commandlineArguments.count("verbose") != 0};
-    const bool EXTRA_VERBOSE{commandlineArguments.count("extra_v") != 0};
-    const float SAFE_DISTANCE{(commandlineArguments.count("sd") != 0) ? static_cast<float>(std::stof(commandlineArguments["sd"])) : static_cast<float>(0.30)};
-    const float MAX_SPEED{(commandlineArguments.count("sp") != 0) ? static_cast<float>(std::stof(commandlineArguments["sp"])) : static_cast<float>(0.15)};
-
-    if (VERBOSE || EXTRA_VERBOSE)
-    {
-        std::cout << "starting up " << argv[0] << "..." << std::endl;
-        std::cout << "speed: [" << MAX_SPEED << "], front saftey distance: [" << SAFE_DISTANCE << " meters]" << std::endl;
-    }
-
-    /**
-     * create a od4session object that will allow all microservices
-     * with the same secret number to send and recieve messages from
-     * one another
-    */
-
-    cluon::OD4Session carlos_session{CARLOS_SESSION}; //needed to send messages to carlos session
-    cluon::OD4Session car_session{CID};               //needed to recieve data from sensors
-
-    if (car_session.isRunning())
-    {
-        if (VERBOSE || EXTRA_VERBOSE)
-        {
-            std::cout << "[acc] micro-service started..." << std::endl;
-        }
-
-        /*set up messages to send*/
-        carlos::acc acc; //send to carlos session
-        float frontSensor = 0.0;
-
-        /*sends front sensor messages to carlos delegator*/
-        auto distanceTrigger = [EXTRA_VERBOSE, VERBOSE, SAFE_DISTANCE, MAX_SPEED, &carlos_session, &acc, &frontSensor](cluon::data::Envelope &&envelope) {
-            /** unpack message recieved*/
-            auto msg = cluon::extractMessage<opendlv::proxy::DistanceReading>(std::move(envelope));
-            /*store sender id*/
-            const uint16_t senderStamp = envelope.senderStamp();
-            /*store sender data*/
-            float distance = msg.distance();
-
-            if (senderStamp == 0)
-            {
-                /**
-                 * only get data fron sender 0 (front sensor)
-                 * */
-
-                /*get front sensor value*/
-                frontSensor = distance;
-                /*calculate speed*/
-                float speed = autoPedal(frontSensor, SAFE_DISTANCE, MAX_SPEED, VERBOSE);
-                /*store speed in carlos object*/
-                acc.speed(speed);
-                acc.front_sensor_is_safe((frontSensor < SAFE_DISTANCE) ? true : false); //if front sensor is safe, set to true, else set to false
-                /*send object to carlos delegator*/
-                carlos_session.send(acc);
-            }
-
-            if (EXTRA_VERBOSE)
-            {
-                std::cout << "autoPedal engaged.." << std::endl;
-            }
-        };
-
-        /*registers callbacks*/
-        car_session.dataTrigger(opendlv::proxy::DistanceReading::ID(), distanceTrigger);
-
-        while (car_session.isRunning())
-        {
-            /* just run this microservice until ist crashes */
-        }
-    }
-    else
-    {
-        std::cout << "Carlos Out. (OD4Session timed out.)" << std::endl;
-    }
-    return 0;
 }
--- microservices/delegator/src/delegator.cpp
+++ microservices/delegator/src/delegator.cpp
@@ -19,16 +19,14 @@ int32_t main(int32_t argc, char **argv)
     if (0 != commandlineArguments.count("help"))
     {
         std::cerr << argv[0] << " is an example application for miniature vehicles (Kiwis) of DIT638 course." << std::endl;
-        std::cerr << "Usage:" << argv[0] << " --cid=<CID of your OD4Session>" << std::endl;
-        std::cerr << argv[0] << "[--carlos=<ID of carlos microservices>]" << std::endl;
+        std::cerr << "Usage:" << argv[0] << "[--carlos=<ID of carlos microservices>]" << std::endl;
         std::cerr << argv[0] << "[--freq=<frequency>]" << std::endl;
         std::cerr << argv[0] << "[--verbose" << std::endl;
         std::cerr << argv[0] << "[--help]" << std::endl;
-        std::cerr << "example:  " << argv[0] << " --cid=112 --carlos=113 --freq=30" << std::endl;
+        std::cerr << "example:  " << argv[0] << "--carlos=113 --freq=30" << std::endl;
         return -1;
     }
     const uint16_t CARLOS_SESSION{(commandlineArguments.count("carlos") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["carlos"])) : static_cast<uint16_t>(113)};
-    const uint16_t CID_SESSION{(commandlineArguments.count("cid") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["cid"])) : static_cast<uint16_t>(112)};
     const float FREQ{(commandlineArguments.count("freq") != 0) ? static_cast<float>(std::stof(commandlineArguments["freq"])) : static_cast<float>(30)};
     const bool VERBOSE{commandlineArguments.count("verbose") != 0};
 
@@ -43,126 +41,94 @@ int32_t main(int32_t argc, char **argv)
      * one another
     */
     cluon::OD4Session carlos_session{CARLOS_SESSION}; //needed to send messages to carlos session
-    cluon::OD4Session car_session{CID_SESSION};       //needed to send messages to car
 
-    if (car_session.isRunning())
+    if (carlos_session.isRunning())
     {
         if (VERBOSE)
         {
             std::cout << "[delegator] micro-service started..." << std::endl;
         }
 
-        /*global variables*/
-        float unassigned = -1.0;
-        bool front_sensor = false;                                    //acc
-        float speed = unassigned, turn = unassigned;                  //acc
-        float center_of_car = unassigned, area_of_car = unassigned;   //car vision
-        float center_of_sign = unassigned, area_of_sign = unassigned; //sign vision
-        uint16_t sign_type = -1;                                      //sign vision
-
+        /*global variables*/ //sign vision
         carlos::semaphore::acc acc_semaphore;
         carlos::semaphore::cmd cmd_semaphore;
         carlos::semaphore::vision::color color_semaphore;
         carlos::semaphore::vision::object object_semaphore;
-
-        opendlv::proxy::PedalPositionRequest pedal;
-        opendlv::proxy::GroundSteeringReading wheel;
+        const bool LOCK = false;
+        const bool UNLOCK = true;
 
         /* prepare messages to recieve from carlos session */
-        auto adaptive_cruise_control = [VERBOSE, &pedal, &speed, &front_sensor](cluon::data::Envelope &&envelope) {
+        auto adaptive_cruise_control = [VERBOSE](cluon::data::Envelope &&envelope) {
             /** unpack message recieved*/
             auto msg = cluon::extractMessage<carlos::acc>(std::move(envelope));
             /*store speed and front_sensor value from acc microservice*/
-            speed = msg.speed();
-            front_sensor = msg.front_sensor_is_safe();
-            /*store speed in car object*/
-            pedal.position(speed);
-
-            if (VERBOSE)
-            {
-                std::cout << "RECIVED -> front sensor distance: [" << front_sensor << "], speed set to [" << speed << "]" << std::endl;
-            }
-        };
-
-        auto user_instruction = [VERBOSE, &wheel, &turn](cluon::data::Envelope &&envelope) {
-            /** unpack message recieved*/
-            auto msg = cluon::extractMessage<carlos::command>(std::move(envelope));
-            /*store turn value from acc microservice*/
-            turn = msg.type();
-            /*store turn in car object*/
-            wheel.groundSteering(turn);
-
-            if (VERBOSE)
-            {
-                std::cout << "RECIVED -> wheel set to [" << turn << "]" << std::endl;
-            }
-        };
-
-        auto car_detection_color = [VERBOSE, /* &wheel, &turn, &pedal, &speed, */ &center_of_car, &area_of_car](cluon::data::Envelope &&envelope) {
-            /** unpack message recieved*/
-            auto msg = cluon::extractMessage<carlos::vision::car>(std::move(envelope));
-            /*store data*/
-            center_of_car = msg.coc();
-            area_of_car = msg.area();
+            bool acc_danger = msg.safe_to_drive();
             /*do stuff with data*/
-            if (VERBOSE)
+
+            if (acc_danger)
             {
-                std::cout << "RECIVED -> coc (wheel): [" << center_of_car << "], area of car: [" << area_of_car << "]" << std::endl;
+                /*[test] lock wheels*/
+                cmd_semaphore.semaphore(LOCK);
+                carlos.send(cmd_semaphore);
+
+                if (VERBOSE)
+                {
+                    std::cout << "LOCKED -> CMD" << std::endl;
+                }
             }
         };
 
-        auto sign_detection_color = [VERBOSE, /* &pedal, &speed, */ &center_of_sign, &area_of_sign, &sign_type](cluon::data::Envelope &&envelope) {
-            /** unpack message recieved*/
-            auto msg = cluon::extractMessage<carlos::vision::sign>(std::move(envelope));
-            /*store data*/
-            center_of_sign = msg.cos();
-            area_of_sign = msg.area();
-            sign_type = msg.type();
-            /*do stuff with data*/
-            if (VERBOSE)
-            {
-                std::cout << "RECIVED -> cos: [" << center_of_sign << "], area of sign: [" << area_of_sign << "]" << std::endl;
-            }
-        };
+        // auto command = [VERBOSE](cluon::data::Envelope &&envelope) {
+        //     /** unpack message recieved*/
+        //     auto msg = cluon::extractMessage<carlos::command>(std::move(envelope));
+        //     /*store turn value from acc microservice*/
+        //     int16_t sign_type = msg.type();
+        //     /*do stuff with date*/
+
+        //     if (VERBOSE)
+        //     {
+        //         std::cout << "RECIVED -> wheel set to [" << turn << "]" << std::endl;
+        //     }
+        // };
+
+        // auto car_detection = [VERBOSE](cluon::data::Envelope &&envelope) {
+        //     /** unpack message recieved*/
+        //     auto msg = cluon::extractMessage<carlos::vision::car>(std::move(envelope));
+        //     /*store data*/
+        //     float center_of_car = msg.coc();
+        //     float area_of_car = msg.area();
+        //     /*do stuff with data*/
+
+        //     if (VERBOSE)
+        //     {
+        //         std::cout << "RECIVED -> coc (wheel): [" << center_of_car << "], area of car: [" << area_of_car << "]" << std::endl;
+        //     }
+        // };
+
+        // auto sign_detection = [VERBOSE](cluon::data::Envelope &&envelope) {
+        //     /** unpack message recieved*/
+        //     auto msg = cluon::extractMessage<carlos::vision::sign>(std::move(envelope));
+        //     /*store data*/
+        //     float center_of_sign = msg.cos();
+        //     float area_of_sign = msg.area();
+        //     int16_t sign_type = msg.type();
+        //     /*do stuff with data*/
+
+        //     if (VERBOSE)
+        //     {
+        //         std::cout << "RECIVED -> cos: [" << center_of_sign << "], area of sign: [" << area_of_sign << "]" << std::endl;
+        //     }
+        // };
 
         /*registers callbacks*/
         carlos_session.dataTrigger(carlos::acc::ID(), adaptive_cruise_control);
-        carlos_session.dataTrigger(carlos::command::ID(), user_instruction);
-        carlos_session.dataTrigger(carlos::vision::car::ID(), car_detection_color);
-        carlos_session.dataTrigger(carlos::vision::sign::ID(), sign_detection_color);
+        // carlos_session.dataTrigger(carlos::command::ID(), command);
+        // carlos_session.dataTrigger(carlos::vision::car::ID(), car_detection);
+        // carlos_session.dataTrigger(carlos::vision::sign::ID(), sign_detection);
 
-        /*prepare time related functions*/
-        auto sendPedalRequest{[VERBOSE, &car_session, &pedal]() -> bool {
-            /*send pedal req object to car*/
-            car_session.send(pedal);
-
-            if (VERBOSE)
-            {
-                std::cout << "SENT -> set speed to [" << pedal.position() << "]" << std::endl;
-            }
-            return true;
-        }};
-
-        auto sendWheelRequest{[VERBOSE, &car_session, &wheel]() -> bool {
-            /*send pedal req object to car*/
-            car_session.send(wheel);
-
-            if (VERBOSE)
-            {
-                std::cout << "SENT -> set wheel to [" << wheel.groundSteering() << "]" << std::endl;
-            }
-            return true;
-        }};
-
-        /*register time triggers*/
-        car_session.timeTrigger(FREQ, sendPedalRequest);
-        car_session.timeTrigger(FREQ, sendWheelRequest);
-
-        while (car_session.isRunning())
+        while (carlos_session.isRunning())
         {
             /* just run this microservice until ist crashes */
-            // car_session.send(pedal);
-            // car_session.send(wheel);
         }
     }
     else

1a2d414244867c3322819b27a86bd5a47efad2fb
ranimkhojah <guskhojra@student.gu.se>
2019-05-16 20:33:32 +0200

    command directions

--- microservices/control/cmd/src/command.cpp
+++ microservices/control/cmd/src/command.cpp
@@ -11,6 +11,8 @@
 #include "cluon-complete.hpp"
 #include "messages.hpp"
 
+
+
 int32_t main(int32_t argc, char **argv)
 {
     /**Parse the arguments from the command line*/
@@ -19,29 +21,21 @@ int32_t main(int32_t argc, char **argv)
     if (0 != commandlineArguments.count("help"))
     {
         std::cerr << argv[0] << " is an example application for miniature vehicles (Kiwis) of DIT638 course." << std::endl;
-        std::cerr << argv[0] << "[--carlos=<ID of carlos microservices>]" << std::endl;
         std::cerr << argv[0] << "[--cid=<ID of KIWI session>]" << std::endl;
-        std::cerr << argv[0] << "[--turn=<turn angle>]" << std::endl;
         std::cerr << argv[0] << "[--verbose] print information" << std::endl;
         std::cerr << argv[0] << "[--help]" << std::endl;
         std::cerr << "example:  " << argv[0] << "--cid=112 --carlos=113 --verbose" << std::endl;
         return -1;
     }
-    const uint16_t CARLOS_SESSION{(commandlineArguments.count("carlos") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["carlos"])) : static_cast<uint16_t>(113)};
     const uint16_t CID_SESSION{(commandlineArguments.count("cid") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["cid"])) : static_cast<uint16_t>(112)};
-    const float TURN{(commandlineArguments.count("turn") != 0) ? static_cast<float>(std::stof(commandlineArguments["turn"])) : static_cast<float>(0.2)};
-    const float SP{(commandlineArguments.count("speed") != 0) ? static_cast<float>(std::stof(commandlineArguments["speed"])) : static_cast<float>(0.14)};
     const uint16_t DELAY{(commandlineArguments.count("delay") != 0) ? static_cast<uint16_t>(std::stof(commandlineArguments["delay"])) : static_cast<uint16_t>(1)};
     const bool VERBOSE{commandlineArguments.count("verbose") != 0};
 
-    float turn = TURN;
-    float speed = SP;
-    uint16_t delay_time = DELAY;
+    uint16_t dir = 12;
 
     if (VERBOSE)
     {
         std::cout << "starting up " << argv[0] << "..." << std::endl;
-        std::cout << "turn: [" << TURN << "]" << std::endl;
     }
 
     /**
@@ -49,7 +43,6 @@ int32_t main(int32_t argc, char **argv)
      * with the same secret number to send and recieve messages from
      * one another
     */
-    cluon::OD4Session carlos_session{CARLOS_SESSION};
     cluon::OD4Session car_session{CID_SESSION};
 
     if (car_session.isRunning())
@@ -61,94 +54,91 @@ int32_t main(int32_t argc, char **argv)
 
         /**
         * set up messages that you might send
+        * "no messages will be sent in this microservice"
         */
 
         //carlos::command cmd;                         //[carlos] turn_type
         opendlv::proxy::GroundSteeringRequest wheel; //[car] groundSteering
         opendlv::proxy::PedalPositionRequest pedal;  //[car] pedal position
-
-        bool SEMAPHORE_KEY = true;
-        //const int16_t left = 2, right = 1, neutral = 0;
-        //const float turnRight = TURN * -1, turnLeft = TURN, turnStraight = 0;
-        //int userInp = -1;
-
-        auto semaphore = [VERBOSE, &SEMAPHORE_KEY](cluon::data::Envelope &&envelope) {
-            /** unpack message recieved*/
-            auto msg = cluon::extractMessage<carlos::semaphore::cmd>(std::move(envelope));
-            /*store data*/
-            SEMAPHORE_KEY = msg.semaphore();
-
-            if (VERBOSE)
-            {
-                std::cout << "RECIEVED -> SEMAPHORE_KEY [" << SEMAPHORE_KEY << "]" << std::endl;
-            }
-        };
-
-        carlos_session.dataTrigger(carlos::semaphore::cmd::ID(), semaphore);
-        /*
+      
         while (car_session.isRunning())
         {
-            std::cout << "press: " << std::endl;
-            std::cout << "[" << left << "] for left turn" << std::endl;
-            std::cout << "[" << right << "] for right turn" << std::endl;
-            std::cout << "[" << neutral << "] for neutral" << std::endl;
-            //take in input
-            scanf("%d", &userInp);
+            std::cout << "Direction (9, 12, 3):" << std::endl;
+            scanf("%d", &dir);
 
-            switch (userInp)
+            switch (dir)
             {
-            case left:
-                cmd.turn_type(left);
-                wheel.groundSteering(turnLeft);
-                std::cout << "Wheel is turning [Left]" << std::endl;
-                break;
-            case right:
-                cmd.turn_type(right);
-                wheel.groundSteering(turnRight);
-                std::cout << "Wheel is turning [Right]" << std::endl;
-                break;
-            case neutral:
-                cmd.turn_type(neutral);
-                wheel.groundSteering(turnStraight);
-                std::cout << "Wheel is [Straight]" << std::endl;
-                break;
-            }
-            //send data
-            if (SEMAPHORE_KEY)
-            {
+                case 9:
+                std::cout << "carlos turning left" << std::endl;
+                //turn wheel
+                wheel.groundSteering(0.14);
                 car_session.send(wheel);
+
+                //speed
+                pedal.position(0.13);
+                car_session.send(pedal);
+
+                //delay
+                std::chrono::milliseconds timer(3); // or whatever
+                std::this_thread::sleep_for(timer);
+
+                //stop vehicle
+                pedal.position(0);
+                car_session.send(pedal);
+
+                //straighten wheel
+                wheel.groundSteering(0);
+                car_session.send(wheel);                
+                break;
+
+                case 12:
+                std::cout << "carlos turning right" << std::endl;
+                //turn wheel
+                wheel.groundSteering(0);
+                car_session.send(wheel);
+
+                //speed
+                pedal.position(0.12);
+                car_session.send(pedal);
+
+                //delay
+                std::chrono::milliseconds timer(3); // or whatever
+                std::this_thread::sleep_for(timer);
+
+                //stop vehicle
+                pedal.position(0);
+                car_session.send(pedal);
+
+                //straighten wheel
+                wheel.groundSteering(0);
+                car_session.send(wheel);
+                break;
+
+                case 3:
+                std::cout << "carlos turning right" << std::endl;
+                //turn wheel
+                wheel.groundSteering(-0.25);
+                car_session.send(wheel);
+
+                //speed
+                pedal.position(0.13);
+                car_session.send(pedal);
+
+                //delay
+                std::chrono::milliseconds timer(3); // or whatever
+                std::this_thread::sleep_for(timer);
+
+                //stop vehicle
+                pedal.position(0);
+                car_session.send(pedal);
+
+                //straighten wheel
+                wheel.groundSteering(0);
+                car_session.send(wheel);
+                break;
             }
 
-            carlos_session.send(cmd);
-        }*/
-        while (true)
-        {
-            std::cout << "turn angle:" << std::endl;
-            scanf("%f", &turn);
-            std::cout << "speed:" << std::endl;
-            scanf("%f", &speed);
-            std::cout << "delay:" << std::endl;
-            scanf("%hd", &delay_time);
-
-            //turn wheel
-            wheel.groundSteering(turn);
-            car_session.send(wheel);
-
-            //speed
-            pedal.position(speed);
-            car_session.send(pedal);
-
-            //delay
-            std::chrono::milliseconds timer(delay_time); // or whatever
-            std::this_thread::sleep_for(timer);
-
-            //stop vehicle
-            pedal.position(0);
-            car_session.send(pedal);
-
-            //straighten wheel
-            wheel.groundSteering(0);
-            car_session.send(wheel);
+            break;
         }
     }
     else

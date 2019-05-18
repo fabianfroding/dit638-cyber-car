b4997e8f50bcb9d4b736ff057ec9d4d47f9d327b
fabianfroding <gusfrodfa@student.gu.se>
2019-05-18 15:59:55 +0200

    stage 2 logic in detector
    Added basics of logic in detector for stage 2
    Not done: logic for left/west car

--- microservices/detection/src/detection.cpp
+++ microservices/detection/src/detection.cpp
@@ -36,7 +36,7 @@ int32_t main(int32_t argc, char **argv)
   //**VARIABLES**//
   int32_t retCode{1};
   bool stopSignPresent=false, stopSignDetected=false, westCar=false, northCar=true, eastCar=false;
-  uint16_t status=0;
+  uint16_t stage = 0; //uint16_t status=0;
   String stopSigns_cascade_name;
   CascadeClassifier stopSigns_cascade;
   vector<vector<Point>> car_contours, car_polygons, stop_contours, stop_polygons;
@@ -108,141 +108,246 @@ int32_t main(int32_t argc, char **argv)
       };
       /*registered callback*/
       carlos_session.dataTrigger(carlos::color::status::ID(), semaphore);
+      
+      // Variables for storing which cars were present before approaching the intersection.
+      bool wasPresentBeforeWest = false;
+      bool wasPresentBeforeNorth = false;
+      bool wasPresentBeforeEast = false;
+      
+      //==============================
+      // CHECK STAGES
+      //==============================
+      
+      //==================== CODE FOR STAGE !
+      if (stage == 1) {
+		 // Variables to get average stop signs detected of every fifth frame.
+		  float framesCounted = 0;
+		  float objectsCounted = 0;
+		  // Variables to get average green colors detected in west, north and east frames for every fifth frame.
+		  float framesCountedColor = 0;
+		  float colorsCountedWest = 0;
+		  float colorsCountedNorth = 0;
+		  float colorsCountedEast = 0;
+		  // Endless loop; end the program by pressing Ctrl-C.
+		  while (carlos_session.isRunning() || kiwi_session.isRunning())
+		  {
+		    // Wait for a notification of a new frame.
+		    sharedMemory->wait();
+		    // Lock the shared memory.
+		    sharedMemory->lock();
+		    {
+		      // Copy image into cvMat structure.
+		      // Be aware of that any code between lock/unlock is blocking
+		      // the camera to provide the next frame. Thus, any
+		      // computationally heavy algorithms should be placed outside
+		      // lock/unlock.
+		      Mat wrapped(HEIGHT - 110, WIDTH, CV_8UC4, sharedMemory->data());
+		      img = wrapped.clone();
+		      img2 = wrapped.clone();
+		    }
+		    sharedMemory->unlock();
 
-	  // Variables to get average stop signs detected of every fifth frame.
-	  float framesCounted = 0;
-      float objectsCounted = 0;
-      // Variables to get average green colors detected in west, north and east frames for every fifth frame.
-      float framesCountedColor = 0;
-      float colorsCountedWest = 0;
-      float colorsCountedNorth = 0;
-      float colorsCountedEast = 0;
-      // Endless loop; end the program by pressing Ctrl-C.
-      while (carlos_session.isRunning() || kiwi_session.isRunning())
-      {
-        // Wait for a notification of a new frame.
-        sharedMemory->wait();
-        // Lock the shared memory.
-        sharedMemory->lock();
-        {
-          // Copy image into cvMat structure.
-          // Be aware of that any code between lock/unlock is blocking
-          // the camera to provide the next frame. Thus, any
-          // computationally heavy algorithms should be placed outside
-          // lock/unlock.
-          Mat wrapped(HEIGHT - 110, WIDTH, CV_8UC4, sharedMemory->data());
-          img = wrapped.clone();
-          img2 = wrapped.clone();
-        }
-        sharedMemory->unlock();
+		    resize(img, resizedImg, Size(static_cast<double>(img.cols) * 0.5, static_cast<double>(img.rows * 0.5)), 0, 0, CV_INTER_LINEAR);
+		    resize(img2, resizedImg2, Size(static_cast<double>(img.cols) * 0.5, static_cast<double>(img2.rows * 0.5)), 0, 0, CV_INTER_LINEAR);
+		    //resizedImg.convertTo(img_higher_brightness, -1, 1, 70); //increase the brightness by 20 for each pixel
+		    cvtColor(resizedImg, img_hsv, CV_BGR2HSV);
+		    cvtColor(resizedImg2, resizedImg2, COLOR_BGR2GRAY);
+		    equalizeHist(resizedImg2, obj_frame); //equalize greyscale histogram
 
-        resize(img, resizedImg, Size(static_cast<double>(img.cols) * 0.5, static_cast<double>(img.rows * 0.5)), 0, 0, CV_INTER_LINEAR);
-        resize(img2, resizedImg2, Size(static_cast<double>(img.cols) * 0.5, static_cast<double>(img2.rows * 0.5)), 0, 0, CV_INTER_LINEAR);
-        //resizedImg.convertTo(img_higher_brightness, -1, 1, 70); //increase the brightness by 20 for each pixel
-        cvtColor(resizedImg, img_hsv, CV_BGR2HSV);
-        cvtColor(resizedImg2, resizedImg2, COLOR_BGR2GRAY);
-        equalizeHist(resizedImg2, obj_frame); //equalize greyscale histogram
-
-        //==============================
-		    // OBJECT DETECTION
 		    //==============================
-        vector<Rect> stopSigns;
-        stopSigns_cascade.detectMultiScale(obj_frame, stopSigns);
-        size_t nStopSigns = stopSigns.size();
-        objectsCounted += (double)nStopSigns;
-		framesCounted++;
+			// OBJECT DETECTION
+			//==============================
+		    vector<Rect> stopSigns;
+		    stopSigns_cascade.detectMultiScale(obj_frame, stopSigns);
+		    size_t nStopSigns = stopSigns.size();
+		    objectsCounted += (double)nStopSigns;
+			framesCounted++;
 
-		if (nStopSigns != 0) {
-	      for (size_t i = 0; i < nStopSigns; i++) {
-	        Point center(stopSigns[i].x + stopSigns[i].width / 2, stopSigns[i].y + stopSigns[i].height / 2);
-	        ellipse(resizedImg, center, Size(stopSigns[i].width / 2, stopSigns[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
-	      }
-		}
-
-		if (framesCounted >= 5) {
-			int avgObjects = int((objectsCounted / 5) + 0.5);
-			cout << "Average objects detected of last 5 frames: " << avgObjects << endl;
-			framesCounted = 0;
-			objectsCounted = 0;
-
-		    stopSignPresent = (0 < avgObjects) ? true : false;
-	    	if (stopSignPresent) {
-	    		stopSignDetected = true;
-	    	}
-	    	if (stopSignPresent && stopSignDetected) {
-	    		signStatus.detected(true);
-	    		signStatus.reached(false);
-	    	} else if (!stopSignPresent && stopSignDetected) {
-	    		signStatus.detected(false);
-	    		signStatus.reached(true);
-	    	}
-	    	if (stopSignDetected) {
-	    		carlos_session.send(signStatus);
-	    	}
-		}
-        cout << "Stop sign present: " << stopSignPresent << "| Stop sign detected: " << stopSignDetected << flush << endl;
-        //==============================
-
-        car_contours = getContours(img_hsv, car_low, car_high);
-        car_polygons.resize(car_contours.size());
-        car_rectangle.resize(car_contours.size());
-
-        //**PROCESS CAR GREEN DETECTION**
-        eastCar=false; northCar=false; westCar=false;
-          for (size_t k = 0; k < car_contours.size(); k++)
-          {
-            approxPolyDP(car_contours[k], car_polygons[k], 3, true); //approximate the curve of the polygon
-            if (arcLength(car_contours[k], false) > 60)
-            {
-              car_rectangle[k] = boundingRect(car_polygons[k]);
-              //printRectangleLocation(car_contours[k], resizedImg); //coordinates and position of the center of each rectangle
-              if(getCenterOfContour(car_contours[k]).x < resizedImg.size().width/100*30) westCar=true;
-              //else westCar=false;
-              if(getCenterOfContour(car_contours[k]).x >= resizedImg.size().width/100*30 && getCenterOfContour(car_contours[k]).x <= resizedImg.size().width/100*65) northCar=true;
-              //else northCar=false;
-              if(getCenterOfContour(car_contours[k]).x > resizedImg.size().width/100*65) eastCar=true;
-              //else eastCar=false;
-            }
-            groupRectangles(car_rectangle, 3, 0.65); //group overlapping rectangles
-            drawRectangle(car_rectangle[k], resizedImg, edge);
-          }
-            cout <<westCar<<" | "<<northCar<<" | "<<eastCar<<flush<<endl;
-            
-            //==================== GET AVERAGE OF COLORS DETECTED (5th frame)
-            colorsCountedWest += (double)westCar;
-            colorsCountedNorth += (double)northCar;
-            colorsCountedEast += (double)eastCar;
-			framesCountedColor++;
-            if (framesCountedColor == 5) {
-				int avgCarsWest = int((colorsCountedWest / 5) + 0.5);
-				int avgCarsNorth = int((colorsCountedNorth / 5) + 0.5);
-				int avgCarsEast = int((colorsCountedEast / 5) + 0.5);
-				cout << "Avg cars west: " << avgCarsWest << endl;
-				cout << "Avg cars north: " << avgCarsNorth << endl;
-				cout << "Avg cars east: " << avgCarsEast << endl;
-				//framesCountedColor = 0;
-				//colorsCountedWest = 0;
-				//colorsCountedNorth = 0;
-				//colorsCountedEast = 0;
-				
-				//send intersection message
-		        intersection_tracker.west(avgCarsWest);
-		        intersection_tracker.north(avgCarsNorth);
-		        intersection_tracker.east(avgCarsEast);
-		        carlos_session.send(intersection_tracker);
+			if (nStopSigns != 0) {
+			  for (size_t i = 0; i < nStopSigns; i++) {
+			    Point center(stopSigns[i].x + stopSigns[i].width / 2, stopSigns[i].y + stopSigns[i].height / 2);
+			    ellipse(resizedImg, center, Size(stopSigns[i].width / 2, stopSigns[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
+			  }
 			}
-			
-			//====================
 
-        //message sending stopped
-        // Display image.
-        if (VERBOSE)
-        {
-          imshow(sharedMemory->name().c_str(), resizedImg);
-          //  cout << "RECIEVED -> SEMAPHORE_KEY [" << SEMAPHORE_KEY << "]" << endl;
-          waitKey(1);
-        }
+			if (framesCounted >= 5) {
+				int avgObjects = int((objectsCounted / 5) + 0.5);
+				cout << "Average objects detected of last 5 frames: " << avgObjects << endl;
+				framesCounted = 0;
+				objectsCounted = 0;
+
+				stopSignPresent = (0 < avgObjects) ? true : false;
+				if (stopSignPresent) {
+					stopSignDetected = true;
+				}
+				if (stopSignPresent && stopSignDetected) {
+					signStatus.detected(true);
+					signStatus.reached(false);
+				} else if (!stopSignPresent && stopSignDetected) {
+					signStatus.detected(false);
+					signStatus.reached(true);
+				}
+				if (stopSignDetected) {
+					carlos_session.send(signStatus);
+				}
+			}
+		    cout << "Stop sign present: " << stopSignPresent << "| Stop sign detected: " << stopSignDetected << flush << endl;
+		    //==============================
+
+		    car_contours = getContours(img_hsv, car_low, car_high);
+		    car_polygons.resize(car_contours.size());
+		    car_rectangle.resize(car_contours.size());
+
+		    //**PROCESS CAR GREEN DETECTION**
+		    eastCar=false; northCar=false; westCar=false;
+		      for (size_t k = 0; k < car_contours.size(); k++)
+		      {
+		        approxPolyDP(car_contours[k], car_polygons[k], 3, true); //approximate the curve of the polygon
+		        if (arcLength(car_contours[k], false) > 60)
+		        {
+		          car_rectangle[k] = boundingRect(car_polygons[k]);
+		          //printRectangleLocation(car_contours[k], resizedImg); //coordinates and position of the center of each rectangle
+		          if(getCenterOfContour(car_contours[k]).x < resizedImg.size().width/100*30) westCar=true;
+		          //else westCar=false;
+		          if(getCenterOfContour(car_contours[k]).x >= resizedImg.size().width/100*30 && getCenterOfContour(car_contours[k]).x <= resizedImg.size().width/100*65) northCar=true;
+		          //else northCar=false;
+		          if(getCenterOfContour(car_contours[k]).x > resizedImg.size().width/100*65) eastCar=true;
+		          //else eastCar=false;
+		        }
+		        groupRectangles(car_rectangle, 3, 0.65); //group overlapping rectangles
+		        drawRectangle(car_rectangle[k], resizedImg, edge);
+		      }
+		        cout <<westCar<<" | "<<northCar<<" | "<<eastCar<<flush<<endl;
+		        
+		        //==================== GET AVERAGE OF COLORS DETECTED (5th frame)
+		        colorsCountedWest += (double)westCar;
+		        colorsCountedNorth += (double)northCar;
+		        colorsCountedEast += (double)eastCar;
+				framesCountedColor++;
+		        if (framesCountedColor == 5) {
+					int avgCarsWest = int((colorsCountedWest / 5) + 0.5);
+					int avgCarsNorth = int((colorsCountedNorth / 5) + 0.5);
+					int avgCarsEast = int((colorsCountedEast / 5) + 0.5);
+					cout << "Avg cars west: " << avgCarsWest << endl;
+					cout << "Avg cars north: " << avgCarsNorth << endl;
+					cout << "Avg cars east: " << avgCarsEast << endl;
+					
+					//send intersection message
+				    intersection_tracker.west(avgCarsWest);
+				    intersection_tracker.north(avgCarsNorth);
+				    intersection_tracker.east(avgCarsEast);
+				    carlos_session.send(intersection_tracker);
+				    
+				    // Store which cars were present and not (so they can be retrieved in stage 2)
+					if (avgCarsWest >= 1) wasPresentBeforeWest = true;
+					if (avgCarsNorth >= 1) wasPresentBeforeNorth = true;
+					if (avgCarsEast >= 1) wasPresentBeforeEast = true;
+				}
+				
+				//====================
+
+		    //message sending stopped
+		    // Display image.
+		    if (VERBOSE)
+		    {
+		      imshow(sharedMemory->name().c_str(), resizedImg);
+		      //  cout << "RECIEVED -> SEMAPHORE_KEY [" << SEMAPHORE_KEY << "]" << endl;
+		      waitKey(1);
+		    }
+		  }
+		  	
       }
+      
+      //==================== CODE FOR STAGE 2
+      // Note: stage 2 does not use object detection since it already passed the stop sign.
+      else if (stage == 2) {
+      	
+      	// TO DO:
+      	// Check frames for north and east car IF THEY WERE PRESENT IN STAGE 1.
+      	
+      	framesCountedColor = 0; // Reset from usage in stage 1.
+      	
+      	// This section is almost identical to the one in stage 1, except it doesnt look for the car in west.
+      	while (carlos_session.isRunning() || kiwi_session.isRunning())
+      	{
+		    sharedMemory->wait();
+		    sharedMemory->lock();
+		    {
+		      Mat wrapped(HEIGHT - 110, WIDTH, CV_8UC4, sharedMemory->data());
+		      img = wrapped.clone();
+		      img2 = wrapped.clone();
+		    }
+		    sharedMemory->unlock();
+
+		    resize(img, resizedImg, Size(static_cast<double>(img.cols) * 0.5, static_cast<double>(img.rows * 0.5)), 0, 0, CV_INTER_LINEAR);
+		    resize(img2, resizedImg2, Size(static_cast<double>(img.cols) * 0.5, static_cast<double>(img2.rows * 0.5)), 0, 0, CV_INTER_LINEAR);
+		    cvtColor(resizedImg, img_hsv, CV_BGR2HSV);
+		    cvtColor(resizedImg2, resizedImg2, COLOR_BGR2GRAY);
+		    equalizeHist(resizedImg2, obj_frame);
+      	
+      		car_contours = getContours(img_hsv, car_low, car_high);
+		    car_polygons.resize(car_contours.size());
+		    car_rectangle.resize(car_contours.size());
+
+		    //**PROCESS CAR GREEN DETECTION**
+		    eastCar=false; northCar=false; westCar=false;
+		      for (size_t k = 0; k < car_contours.size(); k++)
+		      {
+		        approxPolyDP(car_contours[k], car_polygons[k], 3, true);
+		        if (arcLength(car_contours[k], false) > 60)
+		        {
+		          car_rectangle[k] = boundingRect(car_polygons[k]);
+		          if(getCenterOfContour(car_contours[k]).x >= resizedImg.size().width/100*30 && getCenterOfContour(car_contours[k]).x <= resizedImg.size().width/100*65) northCar=true;
+		          if(getCenterOfContour(car_contours[k]).x > resizedImg.size().width/100*65) eastCar=true;
+		        }
+		        groupRectangles(car_rectangle, 3, 0.65); //group overlapping rectangles
+		        drawRectangle(car_rectangle[k], resizedImg, edge);
+		      }
+		        cout <<northCar<<" | "<<eastCar<<flush<<endl;
+		        colorsCountedNorth += (double)northCar;
+		        colorsCountedEast += (double)eastCar;
+				framesCountedColor++;
+		        if (framesCountedColor >= 4) {
+					int avgCarsNorth = int((colorsCountedNorth / 5) + 0.5);
+					int avgCarsEast = int((colorsCountedEast / 5) + 0.5);
+					cout << "Avg cars north: " << avgCarsNorth << endl;
+					cout << "Avg cars east: " << avgCarsEast << endl;
+					
+					// Here we send the status of north and east car depending on if they were present during stage 1.
+					// If a car was not present in stage 1, we dont send anything since we dont care.
+					if (wasPresentBeforeNorth || wasPresentBeforeEast) {
+						if (wasPresentBeforeNorth) {
+							intersection_tracker.north(avgCarsNorth);
+						}
+						if (wasPresentBeforeEast) {
+							intersection_tracker.east(avgCarsEast);
+						}
+						carlos_session.send(intersection_tracker);
+					}
+					
+					// For stage 2, we want to repeatedly check for the cars, unlike in stage 1. So we need to reset the average frame counter.
+					framesCountedColor = 0;
+					avgCarsNorth = 0;
+					avgCarsEast = 0;
+					
+				}
+				
+				//====================
+
+		    //message sending stopped
+		    // Display image.
+		    if (VERBOSE)
+		    {
+		      imshow(sharedMemory->name().c_str(), resizedImg);
+		      //  cout << "RECIEVED -> SEMAPHORE_KEY [" << SEMAPHORE_KEY << "]" << endl;
+		      waitKey(1);
+		    }
+      	
+      	// Here goes logic for the LEFT car.
+      	
+      }
+
+	  
     }
   }
   return retCode;
